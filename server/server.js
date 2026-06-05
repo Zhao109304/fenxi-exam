00af1bef7adf71ca55d3cd6b49d84e1a964c11db
zhh13706005383 <zhh13706005383@163.com>
2026-06-05 21:23:03 +0800

    配置CORS允许GitHub Pages访问

--- server/server.js
+++ server/server.js
@@ -143,7 +143,10 @@ async function saveSubjectData(username, subject, wrongQuestions, examHistory) {
     }
 }
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://zhao109304.github.io', 'http://localhost:8080', 'http://127.0.0.1:8080'],
+    credentials: true
+}));
 app.use(express.json());
 
 let dbInitialized = false;
